9bf1288e8f276061925e33003edc837425ba6e10
Alex <alexandre_2420@outlook.com>
2022-03-25 08:56:31 -0500

    added: collection: Resource

--- src/main/java/com/sofkau/libraryReactive/config/WebFluxConfig.java
+++ src/main/java/com/sofkau/libraryReactive/config/WebFluxConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.sofkau.libraryReactive.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
