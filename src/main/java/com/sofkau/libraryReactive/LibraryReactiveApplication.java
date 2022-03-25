97feadb3e6d5e190d04b398fe501192483deffa4
Alex <alexandre_2420@outlook.com>
2022-03-25 08:52:33 -0500

    added: WebFluxConfig

---
+++ src/main/java/com/sofkau/libraryReactive/LibraryReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.libraryReactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryReactiveApplication.class, args);
+	}
+
+}
